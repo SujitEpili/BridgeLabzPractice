a3291c95a3ee6ae20a981044363626831ba3d159
SujitEpili <sujitkumarepili02@gmail.com>
2024-11-19 11:16:26 +0530

    [SujitEpili] added BubbleSort class

---
+++ src/sortings/BubbleSort.java
@@ -0,0 +1,33 @@
+package sortings;
+
+public class BubbleSort 
+{
+	public static void main(String[] args) 
+	{
+		int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        bubbleSort(arr);
+        System.out.println("Sorted array: ");
+        for (int num : arr) 
+        {
+            System.out.print(num + " ");
+        }
+	}
+	
+	static void bubbleSort(int[] arr)
+	{
+		for(int i=0;i<arr.length-1;i++)
+		{
+			for(int j=0;j<arr.length-1-i;j++)
+			{
+				if(arr[j] > arr[j+1])
+				{
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+			}
+		}
+	}
+
+}
+ 
